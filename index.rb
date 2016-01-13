751bb667a3ff97ebbdcf21355ff70f87843f6b8c
Tracie Wamsley <traciewamsley@gmail.com>
2016-01-13 14:56:31 -0700

    Added empty pages. still need empty crud pages. added find a couple to landing

--- index.rb
+++ index.rb
@@ -16,3 +16,76 @@ get '/home' do
   erb :home
 end
 
+get '/attire' do
+  erb :attire
+end
+
+get '/budget' do
+  erb :budget
+end
+
+get '/checklist' do
+  erb :checklist
+end
+
+get '/color' do
+  erb :color
+end
+
+get '/editCountdown' do
+  erb :editCountdown
+end
+
+get '/examples' do
+  erb :examples
+end
+
+get '/faq' do
+  erb :faq
+end
+
+get '/ideaFolder' do
+  erb :ideaFolder
+end
+
+get '/invite' do
+  erb :invite
+end
+
+get '/registry' do
+  erb :registry
+end
+
+get '/seating' do
+  erb :seating
+end
+
+get '/songs' do
+  erb :songs
+end
+
+get '/themes' do
+  erb :themes
+end
+
+get '/vendors' do
+  erb :vendors
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
